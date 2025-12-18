a7c90dc81c51694f2988f515416ab1cab9023953
humbug <feldolshkolnik@gmail.com>
2025-12-18 14:37:42 +0300

    сделал мегаструктуру

--- document-service/internal/server/handler/zayavlenie_handler.go
+++ document-service/internal/server/handler/zayavlenie_handler.go
@@ -20,17 +20,19 @@ func NewZayavlenieHandler(service *service.ZayavlenieService) *ZayavlenieHandler
 }
 
 func (h *ZayavlenieHandler) CreateZayavlenie(c *gin.Context) {
-	var request models.ZayavlenieRequest
+	var fullRequest models.FullRequest
 
-	if err := c.ShouldBindJSON(&request); err != nil {
+	if err := c.ShouldBindJSON(&fullRequest); err != nil {
 		log.Println(err)
 		c.JSON(http.StatusBadRequest, gin.H{"msg": "invalid parameter request", "err": err.Error()})
 		return
 	}
 
+	request := fullRequest.ZayavlenieData
+
 	dto := mapper.ZayavlenieMapping(request)
 
-	err := h.service.CreateZayavlenie(dto, request.DogovorType)
+	err := h.service.CreateZayavlenie(dto, request.DogovorAgeType)
 	if err != nil {
 		log.Println(err)
 		c.JSON(http.StatusInternalServerError, gin.H{"msg": "creation zayavlenie error", "err": err.Error()})
