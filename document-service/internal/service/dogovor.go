4379a8e58d4db37ce60d40b1858aa2d6936fd21e
humbug <feldolshkolnik@gmail.com>
2025-12-18 15:06:26 +0300

    сделал писос

--- document-service/internal/service/dogovor.go
+++ document-service/internal/service/dogovor.go
@@ -165,7 +165,7 @@ func replacePP3FIZ(doc *docx.Docx, model *dto.DogovorDTO) {
 	doc.Replace("PHONEL", model.ListenerData.ContactPhone, -1)
 	doc.Replace("EMAILL", model.ListenerData.Email, -1)
 
-	seriaNumberGiven := fmt.Sprintf("%s %s, %s", model.Passport.Seria, model.Passport.Number, model.Passport.PassportGiven)
+	seriaNumberGiven := fmt.Sprintf("%s %s, %s", model.Contractor.Passport.Seria, model.Contractor.Passport.Number, model.Contractor.Passport.PassportGiven)
 
 	doc.Replace("SERIAE NUMBERE, GIVENE", seriaNumberGiven, -1)
 
